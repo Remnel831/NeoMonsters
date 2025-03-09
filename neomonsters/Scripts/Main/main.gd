058bc448ae90e98e99785efdc755ce895669fb53
ECrites <156072054+ECrites@users.noreply.github.com>
2025-03-09 01:50:59 -0600

    LastChanges
    Last changes to bring everything together

--- neomonsters/Scripts/Main/main.gd
+++ neomonsters/Scripts/Main/main.gd
@@ -9,3 +9,7 @@ func _on_button_pressed():
 		$Lights.enabled = false
 	else:
 		$Lights.enabled = true
+
+
+func _on_minigame_btn_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Minigames/minigame_1.tscn")
